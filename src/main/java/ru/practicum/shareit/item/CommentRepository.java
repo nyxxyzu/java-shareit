e91cdecfab9791b72957178e03271f140ce5e0c2
nyxxy <gladilinnikita@yandex.ru>
2024-08-28 12:33:48 +0300

    Добавить базу данных с использованием JPA, реализовать функционал бронирований и отзывов.

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Comment;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	@Query(value = "select c.* from comments as c" +
